6ad783ee89ddfa86d6127e6e40e3d1c88be6bd6d
Hirofumi Yoshikawa <hirofumi@users.noreply.github.com>
2023-10-16 01:09:32 +0900

    Add test

--- tests/test.ts
+++ tests/test.ts
@@ -6,3 +6,12 @@ test('monaco editor should load example', async ({ page }) => {
 	await editor.waitFor();
 	await expect(editor).toContainText('[package]');
 });
+
+test('monaco editor should show tooltip', async ({ page }) => {
+	await page.goto('/');
+	await page.locator('.monaco-editor').waitFor();
+	await page.getByText('[package]', { exact: true }).hover();
+	const hover = page.locator('.monaco-hover-content');
+	await hover.waitFor();
+	await expect(hover).toContainText('The only fields required by Cargo are name and version.');
+});
